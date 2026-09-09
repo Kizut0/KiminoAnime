3372adf667372bb5ef64ea461d3289cc59e8574f
aungmyatoogyaw <aungmyatoogyaw@gmail.com>
2026-09-09 13:35:04 +0700

    Initial Commit

---
+++ KiminoAnime/KiminoAnimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiminoAnimeApp.swift
+//  KiminoAnime
+//
+//  Created by Aung Myat Oo Gyaw on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct KiminoAnimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
